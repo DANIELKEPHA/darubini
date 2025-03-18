5aa49640583b573636f929a54282bb877e0bbfab
Daniel Kepha <danielkepha199@gmail.com>
2025-03-18 18:24:53 +0300

    Switched from bcrypt to bcryptjs

--- backend/libs/index.js
+++ backend/libs/index.js
@@ -1,23 +1,22 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
 
 export const hashPassword = async (userValue) => {
   const salt = await bcrypt.genSalt(10);
-
-  const hashedpassword = await bcrypt.hash(userValue, salt);
-  return hashedpassword;
+  const hashedPassword = await bcrypt.hash(userValue, salt);
+  return hashedPassword;
 };
 
 export const comparePassword = async (userPassword, password) => {
   try {
-    const isMatch = await bcrypt?.compare(userPassword, password);
+    const isMatch = await bcrypt.compare(userPassword, password);
     return isMatch;
   } catch (error) {
     console.log(error);
   }
 };
 
-//JSON WEBTOKEN
+// JSON WEB TOKEN
 export function createJWT(id) {
   return JWT.sign({ userId: id }, process.env.JWT_SECRET, {
     expiresIn: "1d",
